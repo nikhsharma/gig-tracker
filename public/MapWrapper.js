893c757a54a0ebdbe229b75ac85bdeef9939d243
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-06-17 12:44:53 +0100

    Adds map that plots markers at venues, as well as basic styling

---
+++ public/MapWrapper.js
@@ -0,0 +1,17 @@
+const MapWrapper = function(element, coords, zoom) {
+  const osmLayer = new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+  this.map = L.map(element)
+  .addLayer(osmLayer)
+  .setView(coords, zoom);
+  this.map.options.minZoom = 2
+}
+
+MapWrapper.prototype.showCity = function(coords) {
+  this.map.flyTo(coords, 11);
+}
+
+MapWrapper.prototype.addMarker = function(coords, venue) {
+  L.marker(coords, {title: venue.name}).addTo(this.map).bindPopup(venue.name);
+  // const marker = L.marker(coords, ).addTo(this.map)
+
+}
--- public/index.js
+++ public/index.js
@@ -2,11 +2,17 @@ const app = function() {
   TicketMasterAPIKey =  // Insert Your API Key Here
   const city = 'Glasgow'
 
+  const mapDiv = document.querySelector('#map');
+  const initial = [0, 0];
+  const zoomLevel = 1;
+  mainMap = new MapWrapper(mapDiv, initial, zoomLevel);
+
+
   const cityButton = document.querySelector('#city');
   cityButton.addEventListener('click', function() {
-
     const city = document.querySelector('#selected-city').value;
-    makeRequest(`https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&size=100&apikey=${TicketMasterAPIKey}`, ticketMasterData);
+    makeRequest(`https://app.ticketmaster.com/discovery/v2/events.json?city=${city}&size=100&sort=date,asc&classificationName=music&apikey=${TicketMasterAPIKey}`, ticketMasterData)
+    ;
   })
 }
 
@@ -21,6 +27,10 @@ const ticketMasterData = function () {
   if (this.status !== 200) return;
   const events = JSON.parse(this.response)['_embedded'].events;
   populateList(events);
+  moveMap(events[0]['_embedded'].venues[0].location);
+  populateMarkers(events);
+  console.log(events);
+  // console.log(Geohash.encode(events[0]['_embedded'].venues[0].location.longitude, events[0]['_embedded'].venues[0].location.latitude,4));
 }
 
 const populateList = function(events) {
@@ -35,14 +45,36 @@ const populateList = function(events) {
 const createIndividualEvent = function(event) {
   const div = document.createElement('div');
   div.classList.add('event')
+
   const artistName = document.createElement('h3');
   artistName.textContent = event.name;
+
   const venue = document.createElement('h4');
   venue.textContent = event['_embedded'].venues[0].name;
 
+  const year = event['dates'].start['localDate'].substr(0,4);
+  const month = event['dates'].start['localDate'].substr(5,2);
+  const day = event['dates'].start['localDate'].substr(8,2);
+  const date = document.createElement('p');
+  date.textContent = `${day}/${month}/${year}`;
+
+
   div.appendChild(artistName);
   div.appendChild(venue);
+  div.appendChild(date);
   return div
+}
+
+const moveMap = function(coords) {
+  mainMap.showCity([coords.latitude, coords.longitude]);
+}
+
+const populateMarkers = function(events) {
+  for (let key in events) {
+    console.log(events[key]['_embedded'].venues[0]);
+    const coords = [events[key]['_embedded'].venues[0].location.latitude, events[key]['_embedded'].venues[0].location.longitude];
+    mainMap.addMarker(coords, events[key]['_embedded'].venues[0]);
+  }
 }
 
 
